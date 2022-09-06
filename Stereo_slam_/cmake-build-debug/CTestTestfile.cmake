104914497775fac46be4073d6b520f1a15a24647
Tosin Oseni <oluwatosinoseni@gmail.com>
2022-09-06 02:49:35 +0100

    removed . files

--- Stereo_slam_/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/emma/dev/visual-slam/Stereo_slam_
-# Build directory: /Users/emma/dev/visual-slam/Stereo_slam_/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- Stereo_slam_/cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/emma/dev/visual-slam/Stereo_slam_/src
-# Build directory: /Users/emma/dev/visual-slam/Stereo_slam_/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
